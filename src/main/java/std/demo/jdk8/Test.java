806f76b30204353acbe763a1f05b255380761432
don.zhang <don.zhang@yeahmobi.com>
2017-05-03 16:51:28 +0800

    update

--- src/main/java/std/demo/jdk8/Test.java
+++ src/main/java/std/demo/jdk8/Test.java
@@ -1,8 +1,6 @@
 package std.demo.jdk8;
 
-import java.time.ZoneId;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -10,7 +8,6 @@ import java.util.stream.Collectors;
 public class Test {
 
 	public static void main(String[] args) {
-
 		// 1.接口有默认实现(default关键字)以及可以含有静态方法
 		// Collection
 
@@ -22,24 +19,27 @@ public class Test {
 		// 3.Lambda 表达式 取代匿名对象方式
 		// 不能脱离上下文单独存在，必须要有一个明确的目标类型，即函数式接口
 
-		// Runnable r = new Runnable() {
-		// @Override
-		// public void run() {
-		// System.out.println();
-		// }
-		// };
-		Runnable r = () -> {
-			System.out.println();
+		Runnable r = new Runnable() {
+			@Override
+			public void run() {
+				System.out.println();
+			}
+		};
+		
+		Runnable run = () -> {
+			//TODO
 		};
 
-		// Comparator<Integer> c = new Comparator<Integer>() {
-		// @Override
-		// public int compare(Integer c1, Integer c2) {
-		// return c1 - c2;
-		// }
-		// };
-		Comparator<Integer> c = (c1, c2) -> {
-			return c1 - c2;
+//		 Comparator<Integer> comp = new Comparator<Integer>() {
+//		 @Override
+//		 public int compare(Integer i1, Integer i2) {
+//		 return i1 - i2;
+//		 }
+//		 };
+		 
+		 
+		Comparator<Integer> comp = (i1, i2) -> {
+			return i1 - i2;
 		};
 
 		// 4.方法与构造函数引用
@@ -56,7 +56,7 @@ public class Test {
 		// Stream 的创建需要指定一个数据源，比如 java.util.Collection的子类，List或者Set，
 		// Map不支持。Stream的操作可以串行执行或者并行执行。
 		// 生成器函数（Generator function）
-		// Stream.generate(Math::random).limit(5).forEach(System.out::println);
+		//Stream.generate(Math::random).limit(5).forEach(System.out::println);
 
 		// Stream基本操作
 		List<Integer> list = Arrays.asList(13, 11, 10, 9, 8, 7, 6, 5, 5, 4, 3,
@@ -108,22 +108,22 @@ public class Test {
 		// 收集器Collector
 		List<Integer> list3 = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 		// 求平均值averagingDouble/averagingInt/averagingLong
-		list3.stream().collect(Collectors.averagingInt(Integer::intValue));
+		list3.stream().collect(Collectors.averagingInt(i->i));
 
-		// 求和summingDouble/summingInt/summarizingLong
-		list3.stream().collect(Collectors.summingInt(Integer::intValue));
+		// 求和summingDouble/summingInt/summingIntLong
+		list3.stream().collect(Collectors.summingInt(i->i));
 
 		// 统计各项 summarizingDouble/summarizingInt/summarizingLong
 		// 返回包含集合信息的对象(包含元素个数，总和，最小值，最大值，平均值)
-		list3.stream().collect(Collectors.summarizingInt(Integer::intValue));
+		list3.stream().collect(Collectors.summarizingInt(i->i));
 		// 结果IntSummaryStatistics
 		// IntSummaryStatistics{count=10, sum=55, min=1, average=5.500000,
 		// max=10}
 
-		// 返回一个ArrayList
+		// toList将流中元素收集到一个ArrayList中并返回
 		list3.stream().collect(Collectors.toList());
 
-		// 分组groupingBy
+		// groupingBy将流中元素分组 返回一个HashMap
 		list3.stream().collect(Collectors.groupingBy((Integer i) -> {
 			if (i > 0 && i < 4) {
 				return "1~3";
@@ -169,6 +169,12 @@ public class Test {
 		list3.stream().map(Object::toString).collect(Collectors.joining());
 		// 结果:
 		// 12345678910
+		
+		
+		//中间操作(map(),filter(),)
+		//中间操作是lazy的 终结操作是eager
+		//调用了终结操作 才会触发中间操作
+		//不会出现多次遍历的情况 JDK会将所有的中间操作合并成一个，这个过程被称为熔断操作
 	}
 
 	@FunctionalInterface
