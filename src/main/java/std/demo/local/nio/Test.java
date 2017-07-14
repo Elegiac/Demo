712461626587e89d4eccf576c6ec65934cf4cf9a
don.zhang <don.zhang@yeahmobi.com>
2017-07-14 18:51:50 +0800

    code update

--- src/main/java/std/demo/local/nio/Test.java
+++ src/main/java/std/demo/local/nio/Test.java
@@ -1,22 +1,84 @@
 package std.demo.local.nio;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.RandomAccessFile;
+import java.io.UnsupportedEncodingException;
+import java.io.WriteAbortedException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.nio.ByteBuffer;
 import java.nio.CharBuffer;
 import java.nio.channels.FileChannel;
+import java.nio.channels.WritableByteChannel;
 import java.nio.charset.Charset;
 import java.nio.charset.CharsetDecoder;
+import java.nio.charset.CharsetEncoder;
 
 public class Test {
 
 	public static void main(String[] args) throws IOException {
 		// method1();
+
+		method2();
+	}
+
+	static void write(WritableByteChannel channel,String msg) throws IOException {
+		Charset charset = Charset.forName("GBK");
+		CharsetEncoder encoder = charset.newEncoder();
+		
+		ByteBuffer bb = ByteBuffer.allocate(5);
+		CharBuffer cb = CharBuffer.allocate(5);
+
+		for (int i = 0; i < msg.length(); i++) {
+			if (cb.hasRemaining()) {
+				cb.put(msg.charAt(i));
+			} else {
+				cb.flip();
+
+				while (cb.hasRemaining()) {
+					encoder.encode(cb, bb, false);
+					bb.flip();
+					
+					channel.write(bb);
+					
+					bb.clear();
+				}
+				
+				cb.clear();
+			}
+		}
+	}
+	
+	
+	
+	
+	
+	
+	public static void method2() throws IOException {
+		
+		File file = new File("C:\\Users\\yeahmobi\\Desktop\\test\\", "test.txt");
+
+		if (!file.getParentFile().exists()) {
+			file.getParentFile().mkdirs();
+		}
+		if (file.exists()) {
+			file.delete();
+		}
+		
+		file.createNewFile();
+		
+		FileInputStream in = new FileInputStream(file);
+		
+		FileChannel fc = in.getChannel();
+		
+		write(fc, "绯闻啊所发生的v覅陪我1213213");
+		
+		in.close();
 	}
 
 	public static void method1() {
@@ -27,8 +89,8 @@ public class Test {
 			RandomAccessFile raf = new RandomAccessFile("C:\\Users\\yeahmobi\\Desktop\\test.txt", "rw");
 			FileChannel fc = raf.getChannel();
 
-			ByteBuffer buffer = ByteBuffer.allocate(512);
-			CharBuffer cb = CharBuffer.allocate(512);
+			ByteBuffer buffer = ByteBuffer.allocate(128);
+			CharBuffer cb = CharBuffer.allocate(128);
 
 			int count = fc.read(buffer);
 			while (count != -1) {
@@ -40,11 +102,12 @@ public class Test {
 					System.out.print(cb.get());
 				}
 				System.out.println();
-				buffer.clear();
-				cb.clear();
+				buffer.compact();
+				cb.compact();
 				count = fc.read(buffer);
 			}
 			raf.close();
+
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
